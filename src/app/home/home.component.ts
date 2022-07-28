262b6adf58ca26934a70a601a9c7353806dc10ad
jayashankar369 <jayashankarmanoj@gmail.com>
2022-07-28 11:21:15 +0530

    fourth commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,11 @@
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { parseClassNames } from '@fullcalendar/angular';
+import { IGetRowsParams } from 'ag-grid-community';
+import { Subscription } from 'rxjs';
+import { TodolistService } from '../service/todolist.service';
 
 @Component({
   selector: 'app-home',
@@ -9,26 +15,60 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
 
-  tasks=['Go to shop','check the washing machine','Water plant','Wash the car','dinner'];
+  //tasks=['Go to shop','check the washing machine','Water plant','Wash the car','dinner'];
   inprogress:any[]=[];
   done:any[]=[];
+  tasks:any=[];
+  newTask:any=[{id:0,category:'',taskName:'',time:'',date:''}];
 
-  constructor() { }
+  
+  rowData:any=[];
 
-  ngOnInit(): void {
-  }
+  constructor(private router:Router,private todoservice:TodolistService,private http:HttpClient) { }
+
+  
   columnDefs = [
-    {headerName: 'Task', field: 'task' },
-    {headerName: 'Time', field: 'time'}
+    {headerName: 'Task', field: 'taskName' },
+    {headerName: 'Category', field: 'category'},
+    {headerName: 'Time', field: 'time'},
+    // {headerName: 'Date', field: 'date'}
 ];
 
-rowData = [
-  { task: 'Go to shop', time: '11:00 am' },
-  { task: 'check the washing machine', time: '1:00 pm' },
-  { task: 'Water plant',  time: '5:30 pm' },
-  { task: 'Wash the car',  time: '3:00 pm' },
-  { task: 'dinner',  time: '8:00 pm' },
-];
+
+
+
+
+ngOnInit(): void {
+    this.todoservice.getTodayList().subscribe((data)=>{
+      console.log(data);
+      console.log("printed");
+      var dataArray=Object.values(data);
+      this.rowData=dataArray;
+      console.log(this.rowData);
+    })
+
+
+    this.todoservice.getTodayTaskName().subscribe((data)=>{
+
+
+      for(let i=0;i<data.length;i++){
+        this.tasks.push(data[i]);
+      }
+    })
+
+  
+    
+    
+    
+}
+
+// rowData = [
+//   { task: 'Go to shop', time: '11:00 am' },
+//   { task: 'check the washing machine', time: '1:00 pm' },
+//   { task: 'Water plant',  time: '5:30 pm' },
+//   { task: 'Wash the car',  time: '3:00 pm' },
+//   { task: 'dinner',  time: '8:00 pm' },
+// ];
 deleteTask(i:number){
   this.tasks.splice(i,1)
 }
